491a96cb94060442fc759869dc7bb9314fec3c05
dev991301 <113315480+dev991301@users.noreply.github.com>
2024-03-27 15:18:41 +1000

    Repeat create/open/stop/dispose of flyleaf players

---
+++ FlyleafCycle/FlyleafPlayer.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using FlyleafLib;
+using FlyleafLib.MediaPlayer;
+
+namespace FlyleafCycle;
+
+public partial class FlyleafPlayer : UserControl
+{
+    public static readonly DependencyProperty IdProperty = DependencyProperty.Register(
+        nameof(Id), typeof(string), typeof(FlyleafPlayer));
+
+    public string Id
+    {
+        get { return (string)GetValue(IdProperty); }
+        set { SetValue(IdProperty, value); }
+    }
+
+    private CancellationTokenSource? _cancel;
+    
+    public FlyleafPlayer()
+    {
+        InitializeComponent();
+    }
+
+    public void StartCycling() 
+    {
+        _cancel?.Cancel();
+        _cancel?.Dispose();
+        _cancel = new CancellationTokenSource();
+
+        var cancel = _cancel.Token;
+        Task.Run(async () =>
+        {
+            try
+            {
+                while (!cancel.IsCancellationRequested)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        LoadPlayer($"file://{AppDomain.CurrentDomain.BaseDirectory}/Media/clip2.mp4");
+                    });
+                    await Task.Delay(20000, cancel);
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        LoadPlayer(string.Empty);
+                    });
+                    await Task.Delay(5000, cancel);
+                }
+            }
+            catch
+            {
+
+            }
+        }, cancel);
+    }
+    
+    public void StopCycling()
+    {
+        _cancel?.Cancel();
+        _cancel?.Dispose();
+        _cancel = null;
+        LoadPlayer(string.Empty);
+    }
+
+    private void LoadPlayer(string uri)
+    {
+        FlyleafPlayer1.Player?.Stop();
+        FlyleafPlayer1.Player?.Dispose();
+
+        if (!string.IsNullOrEmpty(uri))
+        {
+            Config config = new()
+            {
+                Player = new()
+                {
+                    AutoPlay = true,
+                    Usage = Usage.AVS
+                },
+                Audio = new()
+                {
+                    Enabled = false,
+                },
+                Subtitles = new()
+                {
+                    Enabled = false
+                }
+            };
+
+            FlyleafPlayer1.Player = new Player(config);
+            FlyleafPlayer1.Player.Open(uri);
+        }
+    }
+}
---
+++ FlyleafCycle/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using FlyleafLib;
+using Vortice.DXGI.Debug;
+using Vortice.DXGI;
+
+namespace FlyleafCycle;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        if (!Engine.IsLoaded)
+        {
+            Engine.Start(new()
+            {
+                LogLevel = LogLevel.Trace,
+                LogOutput = $"{AppDomain.CurrentDomain.BaseDirectory}\\flyleaf.log",
+                FFmpegPath = $"{AppDomain.CurrentDomain.BaseDirectory}\\FFmpeg"
+            });
+        }
+
+        Task.Run(async () =>
+        {
+            while (true)
+            {
+#if DEBUG
+                Debug.WriteLine($"========= Live objects ==============");
+                try
+                {
+                    if (DXGI.DXGIGetDebugInterface1(out IDXGIDebug1 dxgiDebug).Success)
+                    {
+                        //dxgiDebug.ReportLiveObjects(DXGI.DebugAll, ReportLiveObjectFlags.Summary | ReportLiveObjectFlags.IgnoreInternal);
+                        dxgiDebug.ReportLiveObjects(DXGI.DebugAll, ReportLiveObjectFlags.Summary);
+                        dxgiDebug.Dispose();
+                    }
+
+                }
+                catch { }
+                Debug.WriteLine($"=====================================");
+#endif
+                // Force GC collection to observe a clear linear trend of handle leaks.
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                await Task.Delay(5000);
+            }
+        });
+    }
+
+    private void OnStopCycling(object sender, RoutedEventArgs e)
+    {
+        foreach (var cell in Players.Children)
+        {
+            if (cell is FlyleafPlayer flyleafPlayerCell)
+            {
+                flyleafPlayerCell.StopCycling();
+            }
+        }
+    }
+
+    private void OnStartCycling(object sender, RoutedEventArgs e)
+    {
+        foreach (var cell in Players.Children)
+        {
+            if (cell is FlyleafPlayer flyleafPlayerCell)
+            {
+                flyleafPlayerCell.StartCycling();
+            }
+        }
+    }
+}
